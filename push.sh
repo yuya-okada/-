1db557e30f4d1d181676aba16be41b992b33cf17
yuya-okada <yuyaokada@ld10-Yuya-Okada.local>
2018-04-11 13:51:07 +0900

    5

--- push.sh
+++ push.sh
@@ -1,7 +1,9 @@
 for file in `\find . -maxdepth 1 -type f`; do
+    if echo hogehuga | grep -q fuga; then
+	echo $file'.html'
     pandoc -f markdown -t html $file >> $file + '.html'
 done
 
-git add .
-git commit -m $RANDOM
-git push origin master -u
+# git add .
+# git commit -m $RANDOM
+# git push origin master -u
